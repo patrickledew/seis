457ac3f2ae0c3e85e830dcbb68ddb40da9b010a7
Abdullah Mohammad <fordes12@gmail.com>
2021-06-17 19:33:49 -0500

    added basic layout for Login Page

--- client/src/LoginPage/LoginPage.js
+++ client/src/LoginPage/LoginPage.js
@@ -1,7 +1,15 @@
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import "./loginPage.scss";
 
 const LoginPage = () => {
-  return <div className="LoginPage">Login Page</div>;
+  return (
+    <Paper className="LoginPage fullWidth fullHeight centerVertically centerHorizontally">
+      <Grid item lg={3}>
+        <Paper>xs=3</Paper>
+      </Grid>
+    </Paper>
+  );
 };
 
 export default LoginPage;
