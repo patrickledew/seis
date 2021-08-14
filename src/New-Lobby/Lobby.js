f4307ac06aae2f37147baad515b3b9703fab2476
Abdullah Mohammad <fordes12@gmail.com>
2021-08-14 00:42:07 -0500

    added lobby code sectin in new lobby

--- src/New-Lobby/Lobby.js
+++ src/New-Lobby/Lobby.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Paper, Grid, Button } from "@material-ui/core";
 import "./lobby.css";
 
-function LobbyNew() {
+const LobbyNew = () => {
+  const lobbyCode = "XYZ8G";
   return (
     <Paper className="Lobby centerHorizontally centerVertically">
       <Grid container spacing={3}>
         <Grid item xs={12} className="centerHorizontal">
           <Grid item xs={4} className="lobbyContainer">
-            Testing
+            <div className="textCenter">Lobby Code: {lobbyCode}</div>
           </Grid>
         </Grid>
         <Grid item xs={12} className="centerHorizontal">
@@ -26,6 +27,6 @@ function LobbyNew() {
       </Grid>
     </Paper>
   );
-}
+};
 
 export default LobbyNew;
