0c30744a00cff48c251a95e689c0080a5a0e5a01
Ricky LeDew <adotout1@gmail.com>
2022-07-07 15:55:21 -0400

    Just console.log-ing the env vars

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App/App";
 
+
+console.log("Running in " + process.env.NODE_ENV);
+console.log("PROCESS ENV:" + process.env);
+console.log("IMPORT META ENV:" + import.meta.env);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
